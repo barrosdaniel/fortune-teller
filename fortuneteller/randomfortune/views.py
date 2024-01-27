6851b0b5961f9375bc3da1b90a904d5f5b8d27bc
Daniel Barros <daniel.barros@cqumail.com>
2024-01-27 20:05:30 +0800

    Add fortuneList to views.py

--- fortuneteller/randomfortune/views.py
+++ fortuneteller/randomfortune/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def fortune(request):
     return render(request, 'randomfortune/fortune.html')
+
+fortuneList = [
+   "All will go well with your new project.",
+   "If you continually give, you will continually have.",
+   "Self-knowledge is a life long process.",
+   "You are busy, but you are happy.",
+   "Your abilities are unparalleled.",
+   "Those who care will make the effort.",
+   "Now is the time to try something new.",
+   "Miles are covered one step at a time.",
+   "Don’t just think, act!"
+]
