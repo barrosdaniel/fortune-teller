2d0db020944a91659e92852570758e003c44256b
Daniel Barros <daniel.barros@cqumail.com>
2024-01-27 19:53:33 +0800

    bugfix: Correct app name in views.py.

--- fortuneteller/randomfortune/views.py
+++ fortuneteller/randomfortune/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def fortune(request):
-    return render(request, "ramdomfortune/fortune.html")
+    return render(request, 'randomfortune/fortune.html')
